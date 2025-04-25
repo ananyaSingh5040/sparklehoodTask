6940de9e93269d98c681112e3d2e2f86a7f55d55
Ananya Singh <singhananya5040@gmail.com>
2025-04-26 01:20:52 +0530

    commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const {connectDB}= require("./connect");
+const router = require("./routes/incident")
+const app = express();
+dotenv.config();
+const PORT = process.env.PORT || 8001;;
+connectDB(process.env.MONGO_URI).then(()=>{
+    console.log("MongoDB connected!");
+})
+// middlewares
+app.use(express.json());
+// routes:
+app.use("/api/incidents",router);
+
+app.listen(PORT, "0.0.0.0",()=>{
+    console.log(`Sever running on PORT: ${PORT}`);
+})
